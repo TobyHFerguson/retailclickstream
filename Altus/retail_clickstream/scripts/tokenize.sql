4e8cb871be7019dc888ed0726c7bcae3314e4e05
Toby Ferguson <toby@cloudera.com>
2018-07-02 16:52:07 -0700

    Converted sql script to use variables for file locations

--- Altus/retail_clickstream/scripts/tokenize.sql
+++ Altus/retail_clickstream/scripts/tokenize.sql
@@ -1,3 +1,6 @@
+set original_access_logs='adl://tobyse6.azuredatalakestore.net/Altus/retail_clickstream/original_access_logs';
+set tokenized_access_logs='adl://tobyse6.azuredatalakestore.net/Altus/retail_clickstream/tokenized_access_logs';
+
 drop table if exists intermediate_access_logs;
 CREATE EXTERNAL TABLE intermediate_access_logs (
     ip STRING,
@@ -13,7 +16,7 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.RegexSerDe'
 WITH SERDEPROPERTIES (
     'input.regex' = '([^ ]*) - - \\[([^\\]]*)\\] "([^\ ]*) ([^\ ]*) ([^\ ]*)" (\\d*) (\\d*) "([^"]*)" "([^"]*)"',
     'output.format.string' = "%1$$s %2$$s %3$$s %4$$s %5$$s %6$$s %7$$s %8$$s %9$$s")
-LOCATION 'adl://tobyse6.azuredatalakestore.net/Altus/retail_clickstream/original_access_logs';
+LOCATION ${hiveconf:original_access_logs};
 
 --select * from intermediate_access_logs limit 10;
 
@@ -29,7 +32,7 @@ CREATE EXTERNAL TABLE tokenized_access_logs (
     dash STRING,
     user_agent STRING)
 stored as parquet
-LOCATION 'adl://tobyse6.azuredatalakestore.net/Altus/retail_clickstream/tokenized_access_logs';
+LOCATION ${hiveconf:tokenized_access_logs};
 
 add jar /opt/cloudera/parcels/CDH/lib/hive/lib/hive-contrib.jar;
 INSERT OVERWRITE TABLE tokenized_access_logs SELECT * FROM intermediate_access_logs;
